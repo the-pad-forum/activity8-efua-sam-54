76e0e1bb0332c58cdda113c6734dddbb8f751495
Efua Sam <efuasam577@gmail.com>
2023-12-22 14:44:10 +0000

    initial commit

---
+++ app/component/About.tsx
@@ -0,0 +1,21 @@
+import styles from '../styles/About.module.css';
+
+const About = () => {
+  return (
+    <section className={styles.about}>
+      <h2>About</h2>
+      <div className={styles.aboutVideo}>
+        <video >
+          <source src="" />
+        </video>
+      </div>
+      <div className={styles.aboutText}>{/* Explainer video goes here */}
+      <p >{/* About text goes here */}
+      Lorem ipsum, dolor sit amet consectetur adipisicing elit. Accusantium non aut id ex atque similique eos, deleniti repellendus quibusdam iure quam, quae necessitatibus optio. Temporibus earum suscipit exercitationem mollitia enim.</p>
+      </div>
+      
+    </section>
+  );
+}
+
+export default About;
---
+++ app/page.tsx
@@ -0,0 +1,19 @@
+import Header from './component/Header';
+import About from './component/About';
+import Skills from './component/Skills';
+import Contact from './component/Contact';
+import Footer from './component/Footer';
+
+const Home = () => {
+  return (
+    <>
+      <Header />
+      <About />
+      <Skills />
+      <Contact />
+      <Footer />
+    </>
+  );
+}
+
+export default Home;
